7f05f29da6e0b0b0ccef91fefb551f1b0fabd7f1
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-05-20 20:35:24 -0400

    partial functionality for sidedrawer animation

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -9,13 +9,18 @@ import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return(
         <Auxiliary>
             <Backdrop 
             show={props.open} 
             clicked={props.closed}/>
-                <div className={classes.SideDrawer}>
+                <div className={attachedClasses.join(' ')}>
                     <div className={classes.Logo}>
                         <Logo />
                     </div>
