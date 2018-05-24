fcdbe7cdbc995a9bf40104d9c845b34c89bb3184
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-05-24 17:16:57 -0400

    Added navigation for checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -15,10 +15,10 @@ const checkoutSummary = (props) => {
             </div>
             <Button 
                 btnType="Danger"
-                clicked>CANCEL</Button>
+                clicked={props.checkoutCancelled}>CANCEL</Button>
             <Button 
                 btnType="Success"
-                clicked>Success</Button>
+                clicked={props.checkoutContinued}>Success</Button>
         </div>
     );
 
